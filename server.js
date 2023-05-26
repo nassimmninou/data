efb4522918e4bc18de126fd5de9357dfb0458166
nassimmninou <mninou.nas@gmail.com>
2023-05-26 14:22:48 +0000

    seddamrrr

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+  const express = require("express");
+  const app = express();
+  const cors = require("cors");
 
-app.use(cors());
+  app.use(cors());
 
-const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://ilyasbennane155:ILYASSyoussefNASSIM@cluster0.e5i83it.mongodb.net/restaurantsDB?retryWrites=true&w=majority");
+  const mongoose = require("mongoose");
+  mongoose.connect("mongodb+srv://ilyasbennane155:ILYASSyoussefNASSIM@cluster0.e5i83it.mongodb.net/restaurantsDB?retryWrites=true&w=majority");
 
-const RestaurantModel = require("./models/restaurants");
+  const RestaurantModel = require("./models/restaurants");
 
-app.get("/api/restaurants/:city", async (req, res) => {
-  const Restaurants = await RestaurantModel.find({ ci: req.params.city });
-  res.json(Restaurants);
-});
+  app.get("/api/restaurants/:city", async (req, res) => {
+    const Restaurants = await RestaurantModel.find({ ci: req.params.city });
+    res.json(Restaurants);
+  });
 
-module.exports = app;
+  module.exports = app;
